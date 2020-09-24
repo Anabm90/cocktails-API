4ab651c653223638fe0375f72de0588068670cd7
Ana <63809371+Anabm90@users.noreply.github.com>
2020-09-24 20:39:53 +0200

    Add files via upload

--- models/value.model.js
+++ models/value.model.js
@@ -1,22 +1,23 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-
-const valueSchema = new Schema ({
-     
+const valueSchema = new Schema({
     category: {
         type: [String]
     },
 
-    alcohol : {
+    alcohol: {
         type: [String]
     },
 
     glass: {
         type: [String]
     }
-
-})
+},
+    {
+        timestamps: true
+    }
+)
 
 const Value = mongoose.model("Value", valueSchema)
 
