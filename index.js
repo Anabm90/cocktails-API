5742925409a529dd70af27af5669dec653205099
Ana <63809371+Anabm90@users.noreply.github.com>
2020-09-24 20:45:15 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-
-    // Base URLS
-    app.use('/', require('./base.routes.js'))
-    app.use('/', require('./auth.routes.js'))
-    app.use('/cocktails', require('./cocktails.routes.js'))
-    app.use('/admin', require('./admin.routes.js'))
-}
