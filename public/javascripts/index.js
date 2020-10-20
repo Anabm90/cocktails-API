3ac8492dd9f3114026578247ca22c958af8520cf
Ana <63809371+Anabm90@users.noreply.github.com>
2020-10-20 18:16:26 +0200

    Add files via upload

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,9 @@
 const cocktailsAPI = new APIHandler();
 
 
-//Cocktail random...
-cocktailsAPI.getRandomCocktail()
+
+cocktailsAPI
+  .getRandomCocktail()
   .then(randomCocktail => {
     let text = ''
     randomCocktail.data.drinks.forEach(cocktail => {
@@ -42,25 +43,24 @@ cocktailsAPI.getRandomCocktail()
 let arrayBottles = ['Tequila', 'Champagne', 'Brandy', 'Gin', 'Scotch', 'Jack%20Daniels', 'Rum', 'Baileys%20Irish%20Cream', 'Bourbon', 'Vodka', 'Cointreau', 'Cognac', 'Whiskey', 'Cider', 'Light%20rum', 'Malibu%20Rum', 'Absolut%20citron', 'Amaretto', 'Irish%20whiskey', 'Sambuca']
 
 arrayBottles.map(elem =>
+
   document.getElementById(elem).addEventListener('click', function (event) {
+
     event.preventDefault()
 
     cocktailsAPI
       .getCocktailByIngredient(this.id)
       .then(ingredient => {
         let text = ''
-        ingredient.data.drinks.forEach(cocktails => {
-          text += `<input id='${cocktails.idDrink}' type="image" width="200px" src="${cocktails.strDrinkThumb}" onClick="reply_click()" />`
-        })
+        ingredient.data.drinks.forEach(cocktails => text += `<input id='${cocktails.idDrink}' type="image" width="200px" src="${cocktails.strDrinkThumb}" onClick="reply_click()" />`)
         document.querySelector('.cocktail-ingredients').innerHTML = text
-
       })
       .catch(err => console.log('error!', err))
   })
 )
 
 
-let bottleRecipes
+//let bottleRecipes
 
 function reply_click() {
 
@@ -76,8 +76,8 @@ function reply_click() {
         let ingredients = ''
         let instructions = ''
         detailedCocktail.data.drinks.forEach(recipe => {
-          let index = 1
-          let ingredientArray = []
+
+          let index = 1, ingredientArray = []
 
           while (recipe['strIngredient' + index]) {
             ingredientArray.push({ name: recipe['strIngredient' + index], amount: recipe['strMeasure' + index] ? recipe['strMeasure' + index] : "A dash " });
@@ -90,52 +90,16 @@ function reply_click() {
                     <div class="nameDetails text-center"> <h1>${recipe.strDrink}</h1> </div>
                     <div class="categoryDetails"><strong><p>${recipe.strCategory}</p></strong></div>`
           text += `<div class="ingredientsDetails"><h4>Ingredientes:</h4>`
-
-          ingredientArray.forEach((ingredient) => {
-            text += `<li>${ingredient.amount} of ${ingredient.name}</li>`
-          })
-
+          ingredientArray.forEach(ingredient => text += `<li>${ingredient.amount} of ${ingredient.name}</li>`)
           text += ` </div><div class="instructions">Instrucciones: ${recipe.strInstructions}</div>
                     </div><div class="col-sm-12 col-md-6 col-lg-4">
                     <div id="centered"><img class="image-random" src="${recipe.strDrinkThumb}" alt="imagen"></div>
                     </div></div>`
 
           document.querySelector('.cocktail-recipe').innerHTML = text
-
         })
-
       })
-
       .catch(err => console.log('error!', err))
   })
 }
-
-
-//Listar todos los cocktails
-
-    //   cocktailsAPI.getAllCocktails()
-
-    //       .then(allCocktails => {
-
-    //         let text = ''
-
-
-    //         allCocktails.data.drinks.forEach(cocktail => {
-
-    //           text+=` <div class="cocktail-info">
-    //             <div class="image"><img src="${cocktail.strDrinkThumb}" alt="imagen"></div>
-    //              <div class="name">Name: ${cocktail.strDrink}</div>`
-
-
-    //         document.querySelector('.cocktail-info').innerHTML = text
-
-
-    //         })
-
-    //   })
-
-    //  .catch(err => console.log('error', err))
-
-
-
 
